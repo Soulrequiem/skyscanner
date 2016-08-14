63a7a1b7c3521a0c377ca4f22bb7b8143fe120db
Soul <Soul@SOUL-PC>
2016-08-15 02:39:49 +0900

    Added RouteTables.Route.MapHttpRoute to class

--- skyScanner/skyScanner/Global.asax.cs
+++ skyScanner/skyScanner/Global.asax.cs
@@ -12,6 +12,11 @@ namespace skyScanner
         protected void Application_Start()
         {
             GlobalConfiguration.Configure(WebApiConfig.Register);
+            RouteTable.Routes.MapHttpRoute(
+                name: "DefaultAPI",
+                routeTemplate: "api/airplaneQuoteController/{id}",
+                defaults: new {id = System.Web.Http.RouteParameter.Optional}
+                );
         }
     }
 }
